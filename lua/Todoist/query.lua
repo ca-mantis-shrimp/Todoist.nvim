be17d20b15dcd9eb450962c97b667ad1cca017ff
Darrion Burgess <dargondab9@gmail.com>
2023-07-08 20:52:06 -0700

    fix(query) to use get instead of the put functionality

--- lua/Todoist/query.lua
+++ lua/Todoist/query.lua
@@ -6,11 +6,11 @@ M.url = "https://api.todoist.com/sync/v9/sync"
 
 function M.get_all_projects(api_key)
 	local authorization = "Bearer " .. api_key
-	local data = '{"authorization":"' .. authorization .. '","sync_token":"*","resource_types":["projects"]}'
+	local data = { sync_token = "*", resource_types = "projects" }
 
-	local response = curl.post(M.url, {
-		data = data,
-		headers = { ["Content-Type"] = "application/json" },
+	local response = curl.get(M.url, {
+		auth = authorization,
+		query = data,
 	})
 
 	return vim.fn.json_decode(response)

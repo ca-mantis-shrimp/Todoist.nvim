3172b01a48b79b36da3d222586ca79cfec270c49
Darrion Burgess <dargondab9@gmail.com>
2023-12-28 22:15:36 -0800

    first step of the refactor to make our own, functional, tree model
    after some reflection, i decided that it would be best to imlement my own functioal api to create the tree rather than tinkering with nui.nvim since i have generally preffered functional styles due to the increased ability to test

--- lua/Todoist/model.lua
+++ lua/Todoist/model.lua
@@ -1,47 +1,23 @@
-local tree = require("nui.tree")
-local NuiLine = require("nui.line")
-
 M = {}
 
-M.create_todoist_tree = function(buffer_number, node_list)
-	return tree({
-		bufnr = buffer_number,
-		nodes = node_list,
-		get_node_id = M.get_node_id,
-		prepare_node = M.prepare_node_func,
-	})
-end
+M.convert_projects_to_dictionary = function(projects)
+	local dictionary = {}
 
-M.convert_to_node_list = function(list)
-	local node_list = {}
-
-	for _, value in ipairs(list) do
-		table.insert(node_list, M.convert_to_node(value))
+	for _, project in ipairs(projects) do
+		dictionary[tonumber(project.id)] = {
+			name = project.name,
+			parent_id = project.parent_id,
+			inbox_project = project.inbox_project,
+			collapsed = project.collapsed,
+			color = project.color,
+			child_order = project.child_order,
+			is_archived = project.is_archived,
+			is_deleted = project.is_deleted,
+			view_style = project.view_style,
+		}
 	end
 
-	return node_list
-end
-
-M.convert_to_node = function(node_data)
-	return tree.Node(node_data)
-end
-
-M.prepare_node_func = function(node)
-	local line = sring.rep("  ", node:get_depth() - 1)
-
-	if node:has_children() and node:is_expanded() then
-		local line = line .. "* "
-	elseif not node:is_expanded() then
-		local line = line .. "> "
-	else
-		local line = line .. "  "
-	end
-
-	return line .. "  " .. node.text
-end
-
-M.get_node_id = function(node)
-	return node.id
+	return dictionary
 end
 
 return M
--- tests/model_spec.lua
+++ tests/model_spec.lua
@@ -1,42 +1,63 @@
 local tree_converter = require("Todoist.model")
-local tree = require("nui.tree")
 
 describe("Converting Todoist Types into nui.nvim Tree", function()
 	it("Can convert a single, empty inbox project into a Tree", function()
-		local inbox_project = {
-			is_archived = false,
-			color = "lime_green",
-			shared = false,
-			inbox_project = true,
-			id = "220474322",
-			collapsed = false,
-			child_order = 0,
-			name = "Inbox",
-			is_deleted = false,
-			parent_id = nil,
-			view_style = "list",
+		local inbox_projects = {
+			{
+				is_archived = false,
+				color = "lime_green",
+				shared = false,
+				inbox_project = true,
+				id = "220474322",
+				collapsed = false,
+				child_order = 0,
+				name = "Inbox",
+				is_deleted = false,
+				parent_id = nil,
+				view_style = "list",
+			},
+			{
+				is_archived = false,
+				color = "lime_green",
+				shared = false,
+				inbox_project = false,
+				id = "220474323",
+				collapsed = false,
+				child_order = 1,
+				name = "Another Project",
+				is_deleted = false,
+				parent_id = nil,
+				view_style = "list",
+			},
 		}
 
-		local inbox_node = tree_converter.convert_to_node(inbox_project)
+		local expected_dictionary = {
+			[220474322] = {
+				name = "Inbox",
+				parent_id = nil,
+				inbox_project = true,
+				collapsed = false,
+				color = "lime_green",
+				child_order = 0,
+				is_archived = false,
+				is_deleted = false,
+				view_style = "list",
+			},
+			[220474323] = {
+				name = "Another Project",
+				parent_id = nil,
+				inbox_project = false,
+				collapsed = false,
+				color = "lime_green",
+				child_order = 1,
+				is_archived = false,
+				is_deleted = false,
+				view_style = "list",
+			},
+		}
 
-		for key, _ in ipairs(inbox_project) do
-			assert.is_true(inbox_node[key] ~= nil)
-		end
-	end)
+		local converted_dictionary = tree_converter.convert_projects_to_dictionary(inbox_projects)
 
-	it("Can get the node from a tree using the id function", function()
-		local test_node = tree.Node({ id = "1" })
-
-		local queried_node_id = tree_converter.get_node_id(test_node)
-
-		assert.is_true(queried_node_id == "1")
-	end)
-
-	it("can convert a list of projects to a list of nodes", function()
-		local project_list = { { id = "321", name = "something else" }, { id = "123", name = "Test Project" } }
-
-		local node_list = tree_converter.convert_to_node_list(project_list)
-
-		assert.is_true(#node_list == 2)
+		assert.are.equal(vim.inspect(expected_dictionary), vim.inspect(converted_dictionary))
 	end)
 end)

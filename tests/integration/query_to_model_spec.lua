81d567710431bd7470a5fa804cefa6e2e9796a95
Darrion Burgess <dargondab9@gmail.com>
2024-01-14 19:24:04 -0800

    minor updates to make the whole thing feel cleaner on the tdd format

--- tests/integration/query_to_model_spec.lua
+++ tests/integration/query_to_model_spec.lua
@@ -6,9 +6,8 @@ local util = require("Todoist.util")
 describe("integration between the query module and the todoist model", function()
 	it("should be able to create a node dictionary from a successful query", function()
 		local response = query.get_all_projects("e0007362ae4002e7ce38bc9cf5f17b71f3fc6750", curl)
-		local reduced_response = query.reduce_response(response)
 
-		local node_dictionary = model.create_node_dictionary(reduced_response)
+		local node_dictionary = model.create_node_dictionary(response)
 
 		assert(util.length(node_dictionary) == 5)
 	end)
--- tests/integration/tree_to_display_spec.lua
+++
@@ -1,2 +0,0 @@
-local tree_model = require("Todoist.tree")
-local tree_display = require("Todoist.tree_display")

f8eb2a5d3ff0fd031e7cea96ce90e7189944e116
Darrion Burgess <dargondab9@gmail.com>
2024-01-15 21:36:03 -0600

    i think i got the final api function done

---
+++ lua/Todoist/api.lua
@@ -0,0 +1,28 @@
+local config = require("Todoist.config")
+local curl = require("plenary.curl")
+local query = require("Todoist.query")
+local model = require("Todoist.model")
+local tree = require("Todoist.tree")
+local tree_display = require("Todoist.tree.display")
+local buffer = require("Todoist.buffer")
+local window = require("Todoist.window")
+
+M = {}
+
+M.show_project_list = function()
+	local response = query.get_all_projects(config.api_key, curl)
+
+	local items = model.create_node_dictionary(response)
+
+	local todoist_tree = tree.create_tree(items)
+
+	local tree_lines = tree_display.get_buffer_lines_from_tree(todoist_tree)
+
+	local buffer_id = buffer.create_buffer_with_lines(true, false, tree_lines)
+
+	local window_id = window.create_floating_window(buffer_id)
+
+	return window_id
+end
+
+return M
---
+++ tests/api_spec.lua
@@ -0,0 +1,12 @@
+local config = require("Todoist.config")
+local todoist_api = require("Todoist.api")
+
+describe("creating a todoist window", function()
+	it("can create the todoist floating window", function()
+		config.setup({ api_key = "e0007362ae4002e7ce38bc9cf5f17b71f3fc6750" })
+
+		local todoist_window = todoist_api.show_project_list()
+
+		assert(todoist_window ~= nil)
+	end)
+end)

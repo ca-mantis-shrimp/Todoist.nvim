878f84d043b3b455bae9341906b4e4de47e27871
Darrion Burgess <dargondab9@gmail.com>
2024-02-03 22:32:30 -0800

    added the first working schema and test JSON to validate against

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,6 +1,7 @@
 # Assumes you are running from the root directory of the project
 # Requires the Plenary Plugin to be installed in Neovim
 # and the `tree-sitter` cli is available in your PATH
+# and the `ajv` cli is available in your PATH
 
 # Run Lua Tests
 nvim --headless -c "PlenaryBustedDirectory tests/"
@@ -10,3 +11,6 @@ cd tree-sitter-projects/
 tree-sitter test
 cd ../
 
+# Json Schema Tests
+ajv compile -s schemas/todoist_sync_schema.json --spec=draft2020
+ajv test -s .\schemas\todoist_sync_schema.json -d .\tests\schema\example_todoist_sync_response.json --valid --spec=draft2020

6c4f0548005d146bec3b4575d0f9fb6e09b82dc3
Darrion Burgess <dargondab9@gmail.com>
2024-02-04 11:39:00 -0800

    adding the first steps to adding notes into our code

--- tests/requests_spec.lua
+++ tests/requests_spec.lua
@@ -8,6 +8,7 @@ mock_curl.request = function(request_table)
 			full_synce = true,
 			temp_id_mapping = {},
 			projects = { { id = 1, name = "test_project" } },
+			notes = {},
 			tasks = {},
 		}
 	else
@@ -29,6 +30,7 @@ describe("unit tests for todoist requests", function()
 				full_synce = true,
 				temp_id_mapping = {},
 				projects = { { id = 1, name = "test_project" } },
+				notes = {},
 				tasks = {},
 			}),
 			vim.inspect(projects)
@@ -54,7 +56,7 @@ describe("unit tests for todoist requests", function()
 			temp_id_mapping = {},
 			projects = { { id = 1, name = "test_project" } },
 			items = { { id = 1, content = "test_task" } },
-			tasks = {},
+			notes = { { id = 1, content = "test comment" } },
 		}
 
 		local reduced_response = requests.reduce_response(response)
@@ -63,6 +65,7 @@ describe("unit tests for todoist requests", function()
 			vim.inspect({
 				projects = { { id = 1, name = "test_project" } },
 				items = { { id = 1, content = "test_task" } },
+				notes = { { id = 1, content = "test comment" } },
 			}),
 			vim.inspect(reduced_response)
 		)

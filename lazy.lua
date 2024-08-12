41bc2c01017d12d5eea80162098cc313812ceab2
Darrion Burgess <dargondab9@gmail.com>
2024-08-11 18:35:09 -0700

    adding new stuff

--- lazy.lua
+++ lazy.lua
@@ -1,5 +1,5 @@
 return {
-	"ca-mantis-shrimp/Todoist.nvim",
+	"ca-mantis-shrimp/todoist-nvim",
 	opts = { api_key = vim.env.TODOIST_API_KEY },
 	build = "rockspec",
 }
--- tests/busted.lua
+++ tests/busted.lua
@@ -6,6 +6,6 @@ load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/ma
 require("lazy.minit").busted({
 	spec = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
-		{ "ca-mantis-shrimp/Todoist.nvim" },
+		{ "ca-mantis-shrimp/todoist-nvim" },
 	},
 })
